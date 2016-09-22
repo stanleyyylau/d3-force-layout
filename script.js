c1b25f7287dd5fb51f5e70f1e05bc7b5d382ffef
Stanley <stanleyyylau@gmail.com>
2016-09-22 18:30:06 +0800

    finish the required functionality

--- script.js
+++ script.js
@@ -1,132 +1,91 @@
-var w = 500;
-var h = 500;
-
-var dataset = {
-    nodes: [{
-        name: "Adam"
-    }, {
-        name: "Bob"
-    }, {
-        name: "Carrie"
-    }, {
-        name: "Donovan"
-    }, {
-        name: "Edward"
-    }, {
-        name: "Felicity"
-    }, {
-        name: "George"
-    }, {
-        name: "Hannah"
-    }, {
-        name: "Iris"
-    }, {
-        name: "Jerry"
-    }],
-    edges: [{
-        source: 0,
-        target: 1
-    }, {
-        source: 0,
-        target: 2
-    }, {
-        source: 0,
-        target: 3
-    }, {
-        source: 0,
-        target: 4
-    }, {
-        source: 1,
-        target: 5
-    }, {
-        source: 2,
-        target: 5
-    }, {
-        source: 2,
-        target: 5
-    }, {
-        source: 3,
-        target: 4
-    }, {
-        source: 5,
-        target: 8
-    }, {
-        source: 5,
-        target: 9
-    }, {
-        source: 6,
-        target: 7
-    }, {
-        source: 7,
-        target: 8
-    }, {
-        source: 8,
-        target: 9
-    }]
-};
-
-var force = d3.layout.force()
-    .nodes(dataset.nodes)
-    .links(dataset.edges)
-    .size([w, h])
-    .linkDistance([50]) // New, create distance
-    .charge([-100]) // New, make them repel each other
-    .start();
+var w = 1000;
+var h = 800;
+var dataset = {};
 
 
+d3.json('https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json', function(err, data) {
+    if (err) return err;
+    dataset = data;
+    forceIt();
+})
 
-// Create SVG and append to container
-var svg = d3.select('.container').append('svg').attr('width', w).attr('height', h);
-var edges = svg.selectAll("line")
-    .data(dataset.edges)
-    .enter()
-    .append("line")
-    .style("stroke", "#ccc")
-    .style("stroke-width", 1);
+function forceIt() {
+    var container = d3.select('.container');
+    var forceOpt = {
+        nodes: dataset.nodes,
+        edges: dataset.links,
+        size: [w, h],
+        dis: [50],
+        charge: [-100]
+    }
+    var force = createForce(forceOpt);
+    var imgNode = appendImgNodeToIt(container, dataset.nodes).call(force.drag);
+    var svg = appendSvgToIt(container, w, h);
+    var edges = appendSvgLineToIt(svg, dataset.links);
 
-var nodes = svg.selectAll("circle")
-    .data(dataset.nodes)
-    .enter()
-    .append("circle")
-    .attr("r", 10)
-    .style("fill", function(d, i) {
-        return "#ff0000";
-    })
-    .call(force.drag);
+    edges.style("stroke", "#ccc").style("stroke-width", 1);
+    console.log(imgNode);
+    force.on("tick", function() {
 
-// Show name when node is mouseover
+        edges.attr("x1", function(d) {
+                return d.source.x;
+            })
+            .attr("y1", function(d) {
+                return d.source.y;
+            })
+            .attr("x2", function(d) {
+                return d.target.x;
+            })
+            .attr("y2", function(d) {
+                return d.target.y;
+            });
 
-nodes.on("mouseover", function(d){
-  var $name = d3.select('.show-name');
-  $name.style('display', 'inline-block');
-  $name.style("left", (d3.event.pageX) + "px");
-  $name.style("top", (d3.event.pageY-30) + "px");
-  $name.text(d.name);
-}).on('mouseout', function(d){
-  var $name = d3.select('.show-name');
-  $name.style('display', 'none');
-});
-
-force.on("tick", function() {
-
-    edges.attr("x1", function(d) {
-            return d.source.x;
-        })
-        .attr("y1", function(d) {
-            return d.source.y;
-        })
-        .attr("x2", function(d) {
-            return d.target.x;
-        })
-        .attr("y2", function(d) {
-            return d.target.y;
+        imgNode.style('left', function(d) {
+            return d.x + 'px';
+        });
+        imgNode.style('top', function(d) {
+            return d.y + 'px';
         });
 
-    nodes.attr("cx", function(d) {
-            return d.x;
-        })
-        .attr("cy", function(d) {
-            return d.y;
-        });
+    });
 
-});
+}
+
+
+
+// Utility functions, I use the functional programing style to tackle this issue
+// Below are functions that you pass value to it and return to you
+// The main part is still the d3.json which gets the data and run forceIt on that dataset
+function appendSvgToIt(obj, w, h) {
+    return obj.append('svg').attr('width', w).attr('height', h);
+}
+
+function createForce(opt) {
+    /*
+    The object will look like this
+    opt = {
+      nodes: [],
+      edges: [],
+      size: [w, h],
+      dis: [50], // Default value
+      charge: [-100], // Default value
+    }
+    */
+    return d3.layout.force().nodes(opt.nodes)
+        .links(opt.edges)
+        .size(opt.size)
+        .linkDistance(opt.dis) // <-- New!
+        .charge(opt.charge) // <-- New!
+        .start();
+}
+
+function appendSvgLineToIt(obj, lineData) {
+    return obj.selectAll('line').data(lineData).enter().append('line');
+}
+
+function appendImgNodeToIt(obj, nodeData) {
+    return obj.append('div').attr('class', 'img-node').selectAll('img').data(nodeData).enter().append('img')
+        .attr('class', function(d) {
+            return "flag flag-" + d.code;
+        });
+}
